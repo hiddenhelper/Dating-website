5b4d43a2c7aa91c292d442ff9438687a71308b7a
hiddenhelper <bountyhunter0308@hotmail.com>
2020-02-16 10:55:12 -0800

    master

--- application/views/admin/header.php
+++ application/views/admin/header.php
@@ -8,6 +8,7 @@
   <meta content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no" name="viewport">
 <link rel="shortcut icon" href="">
   <!-- Bootstrap 3.3.7 -->
+  
   <?php $this->load->view('admin/css') ?>
   <?php $this->load->view('admin/js') ?>
 </head>
